b6013b9d77adc3e45de47097a9852f67aa1c9dce
florianbosshard <florian.bosshard@gmail.com>
2013-10-04 22:39:15 +0200

    Added Marker at current position

--- Source/script/map.js
+++ Source/script/map.js
@@ -1,14 +1,40 @@
-$( document ).ready(function() {
-	
-    var map = new L.Map('map', {
-      center: new L.LatLng(47.49901,8.728935),
-      zoom: 17
+var map; 
+function getLocation()
+  {
+  if (navigator.geolocation)
+    {
+    navigator.geolocation.getCurrentPosition(function (position) {
+    	 L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+    }, function (error){
+    	console.log(error);
     });
+    }
+  else{x.innerHTML="Geolocation is not supported by this browser.";}
+  }
 
-    // create a new tile layer
-    var tileUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    layer = new L.TileLayer(tileUrl, {maxZoom: 18});
 
-    // add the layer to the map
-    map.addLayer(layer);
-});
+function showPosition(position) {
+	console.log("Problem?");
+	console.log(position);
+	//var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+}
+
+
+$(document).ready(function() {
+
+	map = new L.Map('map', {
+		center : new L.LatLng(47.49901, 8.728935),
+		zoom : 17
+	});
+
+	// create a new tile layer
+	var tileUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', layer = new L.TileLayer(tileUrl, {
+		maxZoom : 18
+	});
+
+	// add the layer to the map
+	map.addLayer(layer);
+
+
+	getLocation();
+}); 
