b569b03963ecbc7dc33a28d8247a1767807319eb
florianbosshard <florian.bosshard@gmail.com>
2013-10-04 22:11:52 +0200

    Changed to Leaflet.js

--- Source/script/map.js
+++ Source/script/map.js
@@ -1,17 +1,14 @@
 $( document ).ready(function() {
-        var map = new OpenLayers.Map("osMap");
-        var mapnik         = new OpenLayers.Layer.OSM();
-        var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
-        var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
-        // 47.499003,8.728572
-        var position       = new OpenLayers.LonLat(8.728572,47.499003).transform( fromProjection, toProjection);
-        var zoom           = 16; 
- 
- 		console.log(position);
- 
-        map.addLayer(mapnik);
-    	var markers = new OpenLayers.Layer.Markers( "Markers" );
-    	map.addLayer(markers);
-    	markers.addMarker(new OpenLayers.Marker(position));
-        map.setCenter(position, zoom );
+	
+    var map = new L.Map('map', {
+      center: new L.LatLng(47.49901,8.728935),
+      zoom: 17
+    });
+
+    // create a new tile layer
+    var tileUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    layer = new L.TileLayer(tileUrl, {maxZoom: 18});
+
+    // add the layer to the map
+    map.addLayer(layer);
 });
