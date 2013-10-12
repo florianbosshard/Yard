8f2eb2e6b3458dd1d721d79ba1dbf38f436f97c7
florianbosshard <florian.bosshard@gmail.com>
2013-10-13 01:00:17 +0200

    catchMisterX Methode Test-Ajax-Aufruf

--- Source/script/map.js
+++ Source/script/map.js
@@ -1,11 +1,12 @@
 var yard = {
 	map : null,
+	userLocation : null,
 	initMap : function() {
 		map = new L.Map('map', {
 			center : new L.LatLng(47.49901, 8.728935),
-			zoom : 17, 
+			zoom : 17,
 			maxZoom : 19,
-			minZoom: 14
+			minZoom : 14
 		});
 
 		var tileUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', layer = new L.TileLayer(tileUrl);
@@ -15,6 +16,7 @@ var yard = {
 	markUserLocation : function() {
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(function(position) {
+				userLocation = position;
 				ownPositionMarker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
 			}, function(error) {
 				alert(error);
@@ -22,6 +24,33 @@ var yard = {
 		} else {
 			alert("navigator.geolocation ist ausgeschaltet");
 		}
+	},
+	catchMisterX : function() {
+		// send current position to server
+		$.ajax({
+			type : 'PUT',
+			//contentType : 'application/json',
+			contentType: "text/json",
+			url : "/api/",
+			dataType : "json",
+			data : JSON.stringify({
+				"position" : userLocation,
+				"username": "Gewinnertyp"
+			}),
+			success : function(data, textStatus, jqXHR) {
+				console.log(data);
+				alert(data.message);
+
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				console.log(textStatus);
+				console.log(errorThrown);
+			}
+		});
+		// show sanduhr
+
+		// show resultmessage
+
 	},
 	drawNode : function(lat, lon) {
 		var circle = L.circle([lat, lon], 7, {
@@ -49,16 +78,15 @@ $(document).ready(function() {
 	yard.markUserLocation();
 	yard.drawNode(47.499344, 8.726432);
 	var pointA = new L.LatLng(47.499344, 8.726432);
-	var pointB = new L.LatLng(47.498608,8.726545);
-	var pointC = new L.LatLng(47.499304,8.725874);
-	
+	var pointB = new L.LatLng(47.498608, 8.726545);
+	var pointC = new L.LatLng(47.499304, 8.725874);
+
 	var pointList = [pointA, pointB, pointC, pointA];
 	yard.drawPolyLine(pointList);
-	
-	
-	
-	yard.drawNode(47.498608,8.726545);
-	yard.drawNode(47.499304,8.725874);
-	
+
+	yard.drawNode(47.498608, 8.726545);
+	yard.drawNode(47.499304, 8.725874);
+
+	$("#btnCatchMisterX").click(yard.catchMisterX);
 
 });
